b6a6a6631780de4c04986bfea76a1e4376ba7c6f
Luiza Peres <luizanperes@gmail.com>
2024-10-03 23:49:15 +0100

    :

--- python/day_08/django_project/myapp/models.py
+++ python/day_08/django_project/myapp/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib import admin
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'published')
+    list_filter = ('created_at', 'updated_at', 'published')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'created_at'
+
+admin.site.register(Post, PostAdmin)
+
 
 # Create your models here.
